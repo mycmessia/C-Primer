672253c2265b0b826aa716ad00ee3e5459ccc128
myc <164810193@qq.com>
2014-11-07 12:42:28 +0800

    add class Stock

---
+++ stock2.h
@@ -0,0 +1,24 @@
+#ifndef STOCK2_H_
+#define STOCK2_H_
+
+class Stock 
+{
+private:
+	char company[30];
+	int shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val;};
+public:
+	Stock();
+	Stock (const char *co, int n = 0, double pr = 0.0);
+	~Stock() {};
+	void buy (int num, double price);
+	void sell (int num, double price);
+	void update (double price);
+		// const after ) bracket means can't change object's value through *this
+	void show() const; 
+	const Stock &topval(const Stock &s) const;
+};
+
+#endif
